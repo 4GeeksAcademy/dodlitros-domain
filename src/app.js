82c01002561485d42506a873baa5c2dc5987d2d9
Carlos beltran <137569880+DodLitros@users.noreply.github.com>
2023-09-19 23:46:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,52 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = [
+  "el",
+  "nuestro",
+  "tu",
+  "mi",
+  "su",
+  "ese",
+  "cualquier",
+  "algun",
+  "lo",
+  "donde"
+];
+let adj = [
+  "grande",
+  "pequeno",
+  "rápido",
+  "brillante",
+  "innovador",
+  "divertido",
+  "original",
+  "creativo",
+  "interesante",
+  "wiki",
+  "banda"
+];
+let noun = [
+  "restaurante",
+  "blog",
+  "proyecto",
+  "equipo",
+  "sitio",
+  "evento",
+  "viaje",
+  "negocio",
+  "libro",
+  "juego",
+  "programa"
+];
+
+for (let i = 0; i < 11; i++) {
+  console.log(
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+      adj[Math.floor(Math.random() * adj.length)] +
+      noun[Math.floor(Math.random() * noun.length)] +
+      ".com"
+  );
+}
+
